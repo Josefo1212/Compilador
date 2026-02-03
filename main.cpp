28013dfbbdce51620e476e058d0b6bb4aa6d2ced
Laura Ruidiaz <lauravalentinaruar@gmail.com>
2026-02-03 17:44:06 -0400

    se agrego el next y prios en el main para imprimi

--- main.cpp
+++ main.cpp
@@ -15,6 +15,11 @@ int main() {
         if (int* p10 = a.get(10)) cout << "obtener(10) = " << *p10 << "\n";
         if (int* plast = a.last()) cout << "obtener(ultimo) = " << *plast << "\n";
 
+        if (int* n5 = a.next(5)) cout << "next(5) = " << *n5 << "\n";
+        if (int* n0 = a.next(0)) cout << "next(0) = " << *n0 << "\n";
+        if (int* p5 = a.prios(5)) cout << "prios(5) = " << *p5 << "\n";
+        if (int* plastp = a.prios(a.getSize() - 1)) cout << "prios(ultimo) = " << *plastp << "\n";
+
         int removedFirst = 0;
         int removedMiddle = 0;
         int removedLast = 0;
